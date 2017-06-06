fe8c8092b37da9686fe1cbbc231a28abf6cb2752
vitah <vitaliypukhovoy@gmail.com>
2017-06-06 20:44:51 +0300

    EF2

---
+++ Examples/EF2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity.ModelConfiguration;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Data;
+//using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstExistingDatabase
+{
+
+    class Program
+    {
+        // private const string ConnectionString =
+        //    @"Server=.;Database=EntityWhichReferencesSameType;integrated security=SSPI;";
+
+        static void Main(string[] args)
+        {
+
+            Database.SetInitializer<MyContext>(new Initialize());
+
+            Customer headOffice = new Customer();
+            headOffice.CompanyName = "Head Office";
+
+            MyContext context = new MyContext();
+            context.Customers.Add(headOffice);
+            context.SaveChanges();
+
+            //Customer subOffice = new Customer();
+            //subOffice.CompanyName = "Sub Office";
+            //subOffice.HeadOffice = headOffice;
+            //context.Customers.Add(subOffice);
+            //context.SaveChanges();
+
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+        }
+    }
+
+    public class MyContext
+        : DbContext
+    {
+        public MyContext()
+            : base("ExampleOneToMany")
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Configurations.Add(new CustomerConfiguration());
+        //    base.OnModelCreating(modelBuilder);
+        //}
+    }
+
+    public class CustomerConfiguration
+        : EntityTypeConfiguration<Customer>
+    {
+        //public CustomerConfiguration()
+        //    : base()
+        //{
+        //    HasKey(p => p.Id);
+        //    Property(p => p.Id)
+        //        .HasColumnName("Id")
+        //        .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity)
+        //        .IsRequired();
+        //    Property(p => p.CompanyName)
+        //        .HasColumnName("Name")
+        //        .IsRequired();
+        //    HasOptional(p => p.HeadOffice).WithMany()
+        //        .Map(p => p.MapKey("HeadOfficeId"));
+        //    ToTable("Customers");
+        //}
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public Customer HeadOffice { get; set; }
+    }
+
+    public class Initialize : DropCreateDatabaseAlways<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+
+        }
+    }
+
+}
---
+++ Examples/Linq_Group/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] source = { "abc", "hello", "def", "there", "four" };
+            // This time "values" will be an IEnumerable<char>, the first character of each 
+            // source string contributing to the group 
+            var groups = source.GroupBy(x => x.Length,
+                                        x => x, //x[0], 
+                                        (key, values) => key + ":" + string.Join(";", values));
+            foreach (var item in groups)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            List<Person> persons = new List<Person>
+         {
+             new Person { PersonID = 1, car = "Ferrari" },
+             new Person { PersonID = 2, car = "BMW" },
+             new Person { PersonID = 3, car = "Audi" }
+         };
+            //persons[0] = new Person { PersonID = 1, car = "Ferrari" };
+            //persons[1] = new Person { PersonID = 1, car = "BMW" };
+            //persons[2] = new Person { PersonID = 2, car = "Audi" };
+
+            //var results= persons.GroupBy(n => new { n.PersonID, n.car})
+            //    .Select(g => new {
+            //                   g.Key.PersonID,
+            //                   g.Key.car}).ToList();
+
+            var results = persons.GroupBy(p => p.PersonID,
+                               p => p.car,
+                               (key, g) => new
+                               {
+                                   PersonID = key,
+                                   Cars = g.ToList()
+                               }
+                              );
+            foreach (var item in persons)
+            {
+                Console.WriteLine("{0}  {1}", item.PersonID, item.car);
+            }
+
+        }
+
+        class Person
+        {
+            public int PersonID;
+            public string car;
+        }
+
+
+
+
+    }
+
+}
