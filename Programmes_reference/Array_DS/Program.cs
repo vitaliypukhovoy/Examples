383c10c4cd0b6d8d79a2df6ed081a78d47414886
vitah <vitaliypukhovoy@gmail.com>
2017-09-12 14:21:00 +0300

    Hucker Runck

---
+++ Programmes_reference/Array_DS/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] arr_temp = Console.ReadLine().Split(' ');
+
+        var arrRev = arr_temp.Reverse().ToArray();
+        int[] arr = Array.ConvertAll(arrRev, s => int.Parse(s));
+
+        List<int> arr1 = arr.ToList();
+
+        arr1.ForEach(i => Console.Write("{0}\t", i));
+        foreach (var item in arr)
+            Console.Write(item + " ");
+
+
+        //string[] arr_temp = Console.ReadLine().Split(' ');
+
+        //var arrRev = arr_temp[0].Reverse().ToArray();
+        //int[] arr = Array.ConvertAll(arrRev, s => (int)Char.GetNumericValue(s));
+        ////List<int> arr1 = arr.ToList();
+
+        ////arr1.ForEach(i => Console.Write("{0}\t", i));
+        //foreach (var item in arr)
+        //    Console.Write(item + " ");
+
+
+        //int[] numbers = Console
+        //.ReadLine()
+        //.Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+        //.Select(item => int.Parse(item))
+        //.Reverse()
+        //.ToArray();
+
+        //foreach (int item in numbers)
+        //    Console.Write(item);
+    }
+}
+
+
+//4
+//1 4 3 2
---
+++ Programmes_reference/Sparse_Arrays/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparse_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            int tmp = 0;
+            List<string> list = new List<string>();
+           // Dictionary<Tuple<int, string>, int> dic = new Dictionary<Tuple<int, string>, int>();
+            Dictionary<Tuple<int, string>, int> dic = new Dictionary<Tuple<int, string>, int>();
+
+            //string line;
+            //while((line = Console.ReadLine()) != null && line != "")
+            //{
+            //    list.Add(line); 
+            //}
+            list.Add("4");
+            list.Add("aba");
+            list.Add("baba");
+            list.Add("aba");
+            list.Add("xzxb");
+            list.Add("3");
+            list.Add("aba");
+            list.Add("xzxb");
+            list.Add("ab");
+
+            list.ForEach(n =>
+            {
+                bool a = int.TryParse(n, out result);
+                if (a == true)
+                {
+                    tmp = int.Parse(n);
+                }
+                else
+                {
+                    if (dic.ContainsKey(Tuple.Create(tmp, n)))
+                    {
+                        dic[Tuple.Create(tmp, n)]++;
+                    }
+                    else
+                    {
+                        dic.Add(Tuple.Create(tmp, n), 1);
+                    }
+                }
+            });
+
+            var sortDict = dic
+                 .GroupBy(i => i.Key.Item2)
+                 .Select(group => new { elem = group.Key, count = group.Count(), items = group.ToList() })
+                 .ToList();
+            string item = null;
+            foreach (var j in sortDict)
+            {
+
+                if (j.items.Count > 1)
+                {
+                    Console.WriteLine(j.items.Max(i => i.Value));
+                    item = j.elem;
+                }
+                else if (j.items.Count == 1 && j.items[0].Value > 1)
+                    Console.WriteLine(j.items[0].Value);
+
+                else if (j.elem.Contains(item))
+                    continue;
+                else if (j.items.Count == 1 && j.items[0].Value == 1)
+                    Console.WriteLine(0);
+                else if (j.items.Count == 1 && j.items[0].Value >= 1)
+                    Console.WriteLine(0);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+//4
+//aba
+//baba
+//aba
+//xzxb
+//3
+//aba
+//xzxb
+//ab
+//Sample Output
+
+//2
+//1
+//0
